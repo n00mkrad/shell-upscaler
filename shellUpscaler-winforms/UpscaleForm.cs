ec4e9594c69b84b54060aa4b51a13cfce54f49e4
N00MKRAD <me@nmkd.de>
2020-09-12 00:06:05 +0200

    Update UpscaleForm.cs

--- shellUpscaler-winforms/UpscaleForm.cs
+++ shellUpscaler-winforms/UpscaleForm.cs
@@ -21,20 +21,6 @@ namespace shellUpscaler
 
         public UpscaleForm ()
         {
-            /*
-            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
-            {
-                string resourceName = new AssemblyName(args.Name).Name + ".dll";
-                string resource = Array.Find(this.GetType().Assembly.GetManifestResourceNames(), element => element.EndsWith(resourceName));
-
-                using(var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource))
-                {
-                    Byte[] assemblyData = new Byte[stream.Length];
-                    stream.Read(assemblyData, 0, assemblyData.Length);
-                    return Assembly.Load(assemblyData);
-                }
-            };
-            */
             InitializeComponent();
         }
 
